453535c9123e58fd6609609afe8dbf1b0e8f3c46
Nikita <NVnagornyi@gmail.com>
2025-09-13 18:57:06 +0300

    fix bugs 1/2 and add extensions UIColor

---
+++ Tracker/UIColor + ext.swift
@@ -0,0 +1,100 @@
+//
+//  UIColor + ext.swift
+//  Tracker
+//
+//  Created by Никита Нагорный on 13.09.2025.
+//
+
+import UIKit
+
+import UIKit
+
+extension UIColor {
+    static var backgroundTables: UIColor {
+        return UIColor(red: 0.97, green: 0.97, blue: 0.95, alpha: 1.0)
+    }
+    
+    static var backgroundView: UIColor {
+        return UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+    }
+    
+    static var grayCreatingButton: UIColor {
+        return UIColor(red: 0.68, green: 0.68, blue: 0.70, alpha: 1.0)
+    }
+    
+    static var lightGrayYP: UIColor {
+        return UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 1.0)
+    }
+
+    static var redYP: UIColor {
+        return UIColor(red: 0.99, green: 0.30, blue: 0.29, alpha: 1.0)
+    }
+    
+    static var lightOrangeYP: UIColor {
+        return UIColor(red: 1.00, green: 0.53, blue: 0.12, alpha: 1.0)
+    }
+    
+    static var blueYP: UIColor {
+        return UIColor(red: 0.00, green: 0.48, blue: 0.98, alpha: 1.0)
+    }
+    
+    static var purpleYP: UIColor {
+        return UIColor(red: 0.43, green: 0.27, blue: 1.00, alpha: 1.0)
+    }
+    
+    static var greenYP: UIColor {
+        return UIColor(red: 0.20, green: 0.81, blue: 0.41, alpha: 1.0)
+    }
+    
+    static var darkPinkYP: UIColor {
+        return UIColor(red: 0.90, green: 0.43, blue: 0.83, alpha: 1.0)
+    }
+    
+    static var lightPinkYP: UIColor {
+        return UIColor(red: 0.98, green: 0.83, blue: 0.83, alpha: 1.0)
+    }
+    
+    static var lightBlueYP: UIColor {
+        return UIColor(red: 0.20, green: 0.65, blue: 1.00, alpha: 1.0)
+    }
+    
+    static var lightGreenYP: UIColor {             // #46E69D
+        return UIColor(red: 0.27, green: 0.90, blue: 0.62, alpha: 1.0)
+    }
+    
+    static var darkBlueYP: UIColor {
+        return UIColor(red: 0.21, green: 0.20, blue: 0.49, alpha: 1.0)
+    }
+    
+    static var orangeYP: UIColor {
+        return UIColor(red: 1.00, green: 0.40, blue: 0.30, alpha: 1.0)
+    }
+    
+    static var pinkYP: UIColor {
+        return UIColor(red: 1.00, green: 0.60, blue: 0.80, alpha: 1.0)
+    }
+    
+    static var beigeYP: UIColor {
+        return UIColor(red: 0.96, green: 0.77, blue: 0.55, alpha: 1.0)
+    }
+    
+    static var coralBlueYP: UIColor {
+        return UIColor(red: 0.47, green: 0.58, blue: 0.96, alpha: 1.0)
+    }
+    
+    static var darkPurpleYP: UIColor {
+        return UIColor(red: 0.51, green: 0.17, blue: 0.95, alpha: 1.0)
+    }
+    
+    static var fuchsiaYP: UIColor {
+        return UIColor(red: 0.68, green: 0.34, blue: 0.85, alpha: 1.0)
+    }
+    
+    static var lightPurpleYP: UIColor {
+        return UIColor(red: 0.55, green: 0.45, blue: 0.90, alpha: 1.0)
+    }
+    
+    static var emeraldYP: UIColor {
+        return UIColor(red: 0.18, green: 0.82, blue: 0.35, alpha: 1.0)
+    }
+}
